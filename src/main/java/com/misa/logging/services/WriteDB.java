a33e525adab100c054581cc5c0159ea971b59367
thuhuong <thuhuong@DESKTOP-NDT0E4B.net.fpt>
2017-09-29 11:58:33 +0700

    write data on db

---
+++ src/main/java/com/misa/logging/services/WriteDB.java
@@ -0,0 +1,21 @@
+package com.misa.logging.services;
+
+import java.util.List;
+
+import org.bson.Document;
+
+import com.misa.core.pools.MongoPool;
+import com.misa.logging.entity.UserAccess;
+
+public class WriteDB {
+	public static void writeOnDB(List<UserAccess> userAccLi) {
+		for(UserAccess userAcc : userAccLi) {
+			Document doc = new Document();
+			doc.append("NickName", userAcc.getNickName());
+			doc.append("Count", userAcc.getCount());
+			doc.append("Date", userAcc.getLoginDate());
+			
+			MongoPool.log("UserLoginNumbers", doc);
+		}
+	}
+}
