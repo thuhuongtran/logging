f7772e6db1e149fd1804adc59432404669811b91
thuhuong <thuhuong@DESKTOP-VOHQVS6>
2017-09-29 23:09:42 +0700

    hibernate log

--- src/main/java/com/misa/logging/services/WriteDB.java
+++ src/main/java/com/misa/logging/services/WriteDB.java
@@ -6,17 +6,25 @@ import org.bson.Document;
 
 import com.misa.core.pools.MongoPool;
 import com.misa.logging.entity.UserAccess;
+import com.mongodb.MongoException;
 
 public class WriteDB {
+	public static final int MONGODB_DUPLICATE_CODE = 11000;
 	public static void writeOnDB(List<UserAccess> userAccLi) {
 		for(UserAccess userAcc : userAccLi) {
-			Document doc = new Document();
-			doc.append("NickName", userAcc.getNickName());
-			doc.append("Count", userAcc.getCount());
-			doc.append("Date", userAcc.getLoginDate());
-			doc.append("Time", userAcc.getLoginHour());
+			try {
+				Document doc = new Document();
+				doc.append("NickName", userAcc.getNickName());
+				doc.append("Count", userAcc.getCount());
+				doc.append("Date", userAcc.getLoginDate()+" "+userAcc.getLoginHour());
+				MongoPool.log("UserLoginNumbers", doc);
+			}
+			catch(MongoException e) {
+				if(e.getMessage().contains("E11000")) {
+					continue;
+				}
+			}
 			
-			MongoPool.log("UserLoginNumbers", doc);
 		}
 	}
 }
